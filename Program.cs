7e4fce516f25cf89d219863c39b53ba46e20d623
Stuart Carruthers <stu@ironiclensflare.com>
2018-06-08 20:21:48 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace trains
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
