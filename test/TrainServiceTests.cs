0cc89e5032e26799587c8c05da34b5d6eceeb171
Stuart Carruthers <stu@ironiclensflare.com>
2018-06-12 09:09:48 +0100

    Add fake LDBService

--- test/TrainServiceTests.cs
+++ test/TrainServiceTests.cs
@@ -3,6 +3,9 @@ using LDBWS;
 using Moq;
 using NUnit.Framework;
 using trains.services;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace trains.test
 {
@@ -20,17 +23,60 @@ namespace trains.test
             Assert.Throws<ArgumentException>(() => service.GetTrainsTo(crsFrom, crsTo));
         }
 
-        [Ignore("Re-enable this once LDB API is stubbed")]
         [TestCase("NOT", "BHM")]
         [TestCase("BHM", "NOT")]
         public void GetTrainsTo_ValidCrs_ShouldReturnTrains(string crsFrom, string crsTo)
         {
-            var service = new TrainService();
+            var service = new TrainService(GetMockLdbService());
 
             var trains = service.GetTrainsTo(crsFrom, crsTo);
 
             Assert.NotNull(trains);
             Assert.IsNotEmpty(trains);
+        }
+
+        private LDBServiceSoap GetMockLdbService()
+        {
+            var service = new Mock<LDBServiceSoap>();
+
+            var fakeArrivals = new GetArrivalBoardResponse
+            {
+                GetStationBoardResult = new StationBoard1
+                {
+                    trainServices = GetFakeArrivals()
+                }
+            };
+
+            var fakeDepartures = new GetDepartureBoardResponse
+            {
+                GetStationBoardResult = new StationBoard1
+                {
+                    trainServices = GetFakeDepartures()
+                }
+            };
+
+            service.Setup(s => s.GetArrivalBoardAsync(It.IsAny<GetArrivalBoardRequest>())).Returns(Task.FromResult(fakeArrivals));
+            service.Setup(s => s.GetDepartureBoardAsync(It.IsAny<GetDepartureBoardRequest>())).Returns(Task.FromResult(fakeDepartures));
+
+            return service.Object;
+        }
+
+        private ServiceItem2[] GetFakeArrivals()
+        {
+            using (var reader = new StreamReader("arrivals.json"))
+            {
+                var json = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<ServiceItem2[]>(json);
+            }
+        }
+
+        private ServiceItem2[] GetFakeDepartures()
+        {
+            using (var reader = new StreamReader("departures.json"))
+            {
+                var json = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<ServiceItem2[]>(json);
+            }
         }
     }
 }
